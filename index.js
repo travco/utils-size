f53743984a12f4f9ff014927d844113879fd02c1
Travco <hofnug@gmail.com>
2016-07-25 10:59:12 -0400

    Initial - Convert to dynamically generated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('./lib/size.css');
-require('./lib/size-sm.css');
-require('./lib/size-md.css');
-require('./lib/size-lg.css');
+require('./lib/size-media.css');
+// require('./lib/size-sm.css');
+// require('./lib/size-md.css');
+// require('./lib/size-lg.css');
